50dadeb7a64e570ddc8af75d6034bee543df7bf7
harshithaja <100840741+harshithaja@users.noreply.github.com>
2022-12-20 23:28:29 +0530

    Intial Commit of main Springboot class

---
+++ src/test/java/com/mav/bank/user/MavbankapplicationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mav.bank.user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MavbankapplicationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
